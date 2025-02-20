622bdaed472093ed8938b6c6b28431e2c6d2db28
torrex508 <gonzalotorresgonzalezblanco@gmail.com>
2025-02-20 19:10:14 +0100

    Update kodoland_cap_2.py

--- kodoland_cap_2.py
+++ kodoland_cap_2.py
@@ -1,1 +1,9 @@
+import random
 
+listaLetras = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+numeroLetras = int(input("Introduce el número de letras: "))
+contraseña = ""
+for i in range(numeroLetras):
+    letra = random.choice(listaLetras)
+    contraseña = contraseña + letra
+print(contraseña)
